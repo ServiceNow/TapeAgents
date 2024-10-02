2511a3cc5ee78b1c1c3e36fc0eb663b3df0ae625
rizar <dimabv@tut.by>
2024-10-02 11:06:06 -0400

    fix writing too much

--- examples/optimize.py
+++ examples/optimize.py
@@ -1,4 +1,8 @@
 import logging
+import os
+
+import hydra
+from omegaconf import DictConfig
 
 from tapeagents.io import load_tapes, save_tapes
 from tapeagents.observe import retrieve_all_llm_calls, retrieve_tape_llm_calls
@@ -74,7 +78,7 @@ def make_rag_function_template() -> LLMFunctionTemplate:
     )        
 
 
-def make_rag_agent_and_env() -> tuple[Agent, ToolEnvironment]:
+def make_rag_agent_and_env(cfg: DictConfig) -> tuple[Agent, ToolEnvironment]:
     env = ToolEnvironment(tools=[retrieve])
     
     class RetrieveNode(Node):
@@ -96,43 +100,58 @@ def make_rag_agent_and_env() -> tuple[Agent, ToolEnvironment]:
             LLMFunctionNode(template_name="rag", input_offsets=[-1, -3])
         ],
     )
+    if not cfg.rag.partial_demos:
+        agent.templates["rag"].partial_demos = []   
+    if not cfg.rag.demos:
+        agent.templates["rag"].demos = []
     return agent, env
 
 
-def run_few_shot_rag_agent():
-    agent, env = make_rag_agent_and_env()
+def run_few_shot_rag_agent(cfg: DictConfig):
+    agent, env = make_rag_agent_and_env(cfg)
     start_tape = DialogTape(steps=[UserStep(content="At My Window was released by which American singer-songwriter?")])
     final_tape = main_loop(agent, start_tape, env).get_final_tape()
     print(final_tape.model_dump_json(indent=2))
     
     
-def evaluate_few_shot_rag_agent():
-    agent, env = make_rag_agent_and_env()
+def evaluate_few_shot_rag(cfg: DictConfig):
+    agent, env = make_rag_agent_and_env(cfg)
     logger.info("Loading data")
-    dataset = HotPotQA(train_seed=1, train_size=20, eval_seed=2023, dev_size=50, test_size=0)
+    dataset = HotPotQA(train_seed=1, train_size=20, eval_seed=2023, dev_size=cfg.dataset.dev_size, test_size=0)
     logger.info("Data loaded")
     
     start_tapes = [DialogTape(steps=[UserStep(content=example["question"])]) for example in dataset.dev]
-    final_tapes = list(batch_main_loop(agent, start_tapes, env))
-    
+    final_tapes = []
+    with save_tapes(pathlib.Path("few_shot_rag_tapes.yaml")) as saver:
+        for tape in tqdm.tqdm(batch_main_loop(agent, start_tapes, env)):
+            final_tapes.append(tape)
+            saver.save(tape)
+        
     n_correct = 0
-    for example, final_tape in tqdm.tqdm(zip(dataset.dev, final_tapes)):
+    for example, final_tape in zip(dataset.dev, final_tapes):
         if isinstance(answer := final_tape.steps[-1], AssistantStep):
             ok = answer_exact_match(example, dspy.primitives.Example({'answer': answer.content}))
-            print(example.answer, answer.content, ok)
+            # print(example.answer, answer.content, ok)
             n_correct += int(ok)
     print(f"Accuracy: {n_correct / len(dataset.dev):.2%}")
-    
-    with save_tapes(pathlib.Path("few_shot_rag_tapes.yaml")) as saver:
-        for tape in final_tapes:
-            saver.save(tape)
        
         
-def browse_few_shot_rag_tapes():
+def browse_tapes(): 
     tape_loader = lambda path: load_tapes(DialogTape, path)    
     browser = TapeBrowser(tape_loader, ".", PrettyRenderer())
     browser.launch()
     
+    
+@hydra.main(version_base=None, config_path="../conf/tapeagent", config_name="hotpot_qa")
+def main(cfg: DictConfig):
+    print(f"Running in {os.getcwd()}")
+    match cfg.target:
+        case "evaluate_fewshot_rag":
+            evaluate_few_shot_rag(cfg)
+        case "browse_tapes":
+            browse_tapes()
+        case _:
+            raise ValueError(f"Unknown target {cfg.target}")
+    
 if __name__ == "__main__":
-    evaluate_few_shot_rag_agent()
-    browse_few_shot_rag_tapes()
+    main()
