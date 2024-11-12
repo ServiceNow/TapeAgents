8a0312843d272efbcf7eb66da60b4e25bd22c815
Jordan Prince Tremblay <jordanprince.t@servicenow.com>
2024-11-12 14:23:25 -0500

    simplify code and rename to tape_browser.py

---
+++ examples/data_science/tape_browser.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+import sys
+
+from tapeagents.renderers.camera_ready_renderer import CameraReadyRenderer
+from tapeagents.tape_browser import TapeBrowser
+from tapeagents.team import TeamTape
+
+tape_dir = Path(sys.argv[1])
+os.environ["TAPEAGENTS_SQLITE_DB"] = os.path.join(tape_dir, "tapedata.sqlite")
+
+
+browser = TapeBrowser(TeamTape, sys.argv[1], CameraReadyRenderer(), file_extension=".json")
+browser.launch(port=7680 if len(sys.argv) < 3 else int(sys.argv[2]))
