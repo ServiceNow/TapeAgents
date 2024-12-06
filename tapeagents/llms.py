20f685d537a33a69d4ea91e32966d7d59b9d7a7c
Alexandre Piché <alexandrelpiche@gmail.com>
2024-12-06 12:15:08 -0500

    Update tapeagents/llms.py
    Co-authored-by: Dzmitry Bahdanau <dzmitry.bahdanau@servicenow.com>

--- tapeagents/llms.py
+++ tapeagents/llms.py
@@ -539,7 +539,8 @@ class TrainableLLM(CachedLLM):
                 content = data["choices"][0]["message"]["content"]
                 if self.remove_leading_white_space:
                     # vllm sometimes adds a whitespace at the beginning of the completion
-                    content = content.lstrip()
+assert content[0] == ' '
+content = content[1:]
                 if not content:
                     logger.warning(f"Empty completion {data}")
 
