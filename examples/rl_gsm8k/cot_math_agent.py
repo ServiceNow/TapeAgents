d284c05083ca44006ac2d2fd756a01164a4eef7d
Alexandre Piche <alexandrelpiche@gmail.com>
2024-12-16 16:29:36 +0000

    clean up

--- examples/rl_gsm8k/cot_math_agent.py
+++ examples/rl_gsm8k/cot_math_agent.py
@@ -55,7 +55,7 @@ RLMathTape = Tape[
 class ReasoningNode(MonoNode):
     def parse_completion(self, completion: str, prompt_id: str) -> Generator[Step, None, None]:
         try:
-            step: Union[Task, ReasoningThought, LLMOutputParsingFailureAction] = ReasoningThought(reasoning=completion)
+            step = ReasoningThought(reasoning=completion)
         except Exception as e:
             logger.info(f"Failed to parse agent output: {completion}\n\nError: {e}")
             yield LLMOutputParsingFailureAction(
