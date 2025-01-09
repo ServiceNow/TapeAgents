8f28e46febdb77e6f12f44eae50dba109dfd1922
Alexandre Piche <alexandrelpiche@gmail.com>
2025-01-09 20:07:39 +0000

    -log 1e-8

--- tapeagents/finetune/rl/__init__.py
+++ tapeagents/finetune/rl/__init__.py
@@ -108,7 +108,8 @@ def rl_step(model: PreTrainedModel, batch: dict, config: RLConfig) -> tuple[torc
     log_p_weights = advantages if config.use_advantages else rewards
     log_p_weights = torch.clamp(log_p_weights, min=0) if config.relu_log_p_weights else log_p_weights
     # Second compute the approximated KL, see https://arxiv.org/pdf/2402.03300 eq 4
-    log_ratio_ref_new = torch.clamp(ref_logprobs - new_log_probs, -10, 10)
+    # log (p/(q+1e-8)) = log p - log q - log(1e-8)
+    log_ratio_ref_new = ref_logprobs - new_log_probs - torch.log(torch.tensor([1e-8]))
     approx_kl = torch.exp(log_ratio_ref_new) - log_ratio_ref_new - 1  # Schulman KL approx
     match config.algo:
         case "grpo":
