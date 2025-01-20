8888718139d5c08aec133369a80cb06ae2d3bd73
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-01-20 17:17:22 +0100

    use new sandbox init in gaia

--- examples/gaia_agent/environment.py
+++ examples/gaia_agent/environment.py
@@ -3,7 +3,6 @@ import logging
 from tapeagents.environment import ToolCollectionEnvironment
 from tapeagents.tools.browser import Browser
 from tapeagents.tools.code_executor import CodeExecutor
-from tapeagents.tools.document_reader import DocumentReader
 from tapeagents.tools.media_reader import VideoReader
 from tapeagents.tools.search import Search
 from tapeagents.tools.simple_browser import SimpleBrowser
