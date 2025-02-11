4ee6dbc3f94f5c2a48bda5eb5052451779987520
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-02-11 19:58:28 +0100

    tmp home dir for each container

--- tapeagents/tools/computer/run.sh
+++ tapeagents/tools/computer/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-podman run -p 3000:3000 -p 8000:8000 --mount type=bind,source=$(pwd)/home,target=/config -it computer /bin/bash -c "HOME=/config DISPLAY=:1 python3 /api.py"
+rm -rf .computer_home
+cp -r "$(dirname "$0")/home" .computer_home
+podman run -p 3000:3000 -p 8000:8000 --mount type=bind,source=$(pwd)/.computer_home,target=/config -it computer /bin/bash -c "HOME=/config DISPLAY=:1 python3 /api.py"
