615ac80b38f73f0c0ef84e0f52dd91357718d43b
Nicolas Gontier <gontier.nicolas@yahoo.fr>
2025-02-13 16:05:07 +0000

    cleanup

--- examples/rl_webagent/agent.py
+++ examples/rl_webagent/agent.py
@@ -13,30 +13,30 @@ from .steps import (
 )
 
 
-class WebNode(MonoNode):
-    system_prompt: str = PromptRegistry.system_prompt
-    steps_prompt: str = PromptRegistry.allowed_steps
-    agent_steps: type[Step] | tuple[type[Step], ...] = Field(exclude=True, default=WebAgentStep)
+# class WebNode(MonoNode):
+#     system_prompt: str = PromptRegistry.system_prompt
+#     steps_prompt: str = PromptRegistry.allowed_steps
+#     agent_steps: type[Step] | tuple[type[Step], ...] = Field(exclude=True, default=WebAgentStep)
 
-    def prepare_tape(self, tape: WebTape, max_chars: int = 100):
-        """
-        Trim all page observations except the last two.
-        """
-        tape = super().prepare_tape(tape)  # type: ignore
-        page_positions = [i for i, step in enumerate(tape.steps) if isinstance(step, PageObservation)]
-        if len(page_positions) < 2:
-            return tape
-        prev_page_position = page_positions[-2]
-        steps = []
-        for step in tape.steps[:prev_page_position]:
-            if isinstance(step, PageObservation):
-                short_text = f"{step.text[:max_chars]}\n..." if len(step.text) > max_chars else step.text
-                new_step = step.model_copy(update=dict(text=short_text))
-            else:
-                new_step = step
-            steps.append(new_step)
-        trimmed_tape = tape.model_copy(update=dict(steps=steps + tape.steps[prev_page_position:]))
-        return trimmed_tape
+#     def prepare_tape(self, tape: WebTape, max_chars: int = 100):
+#         """
+#         Trim all page observations except the last two.
+#         """
+#         tape = super().prepare_tape(tape)  # type: ignore
+#         page_positions = [i for i, step in enumerate(tape.steps) if isinstance(step, PageObservation)]
+#         if len(page_positions) < 2:
+#             return tape
+#         prev_page_position = page_positions[-2]
+#         steps = []
+#         for step in tape.steps[:prev_page_position]:
+#             if isinstance(step, PageObservation):
+#                 short_text = f"{step.text[:max_chars]}\n..." if len(step.text) > max_chars else step.text
+#                 new_step = step.model_copy(update=dict(text=short_text))
+#             else:
+#                 new_step = step
+#             steps.append(new_step)
+#         trimmed_tape = tape.model_copy(update=dict(steps=steps + tape.steps[prev_page_position:]))
+#         return trimmed_tape
 
 
 class WebAgent(Agent):
