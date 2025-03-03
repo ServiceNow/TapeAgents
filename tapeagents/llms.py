151d6b9b55bf9a26083c746c72c4902fbf2ec3de
Alexandre Piche <alexandrelpiche@gmail.com>
2025-03-03 23:49:12 +0000

    fix token

--- tapeagents/llms.py
+++ tapeagents/llms.py
@@ -724,7 +724,7 @@ class TrainableLLM(CachedLLM):
                     ]
                     logprobs = self.make_llm_call_logprobs(prompt_token_ids[i], chat_completion_logprobs)
                     chat_completion_tokens = [
-                        int(token["token"].split(":")[-1]) for token in completion_logprobs["tokens"]
+                        int(token["token"].split(":")[-1]) for token in chat_completion_logprobs[1:]
                     ]
                     logprobs = self.get_logprobs_token_ids(prompt_token_ids[i], chat_completion_tokens)
                     # <end_of_turn> is the end of message for Gemma2B, eos_token is wrong for this model
