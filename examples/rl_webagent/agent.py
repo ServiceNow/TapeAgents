df90a4bdab693dc8388e75915fd5b39994d59889
Nicolas Gontier <gontier.nicolas@yahoo.fr>
2025-03-07 22:27:53 +0000

    update agent based on StandardNode

--- examples/rl_webagent/agent.py
+++ examples/rl_webagent/agent.py
@@ -1,62 +1,50 @@
 from itertools import takewhile
-from typing import Any, Generator
+import json
+from typing import Generator
 
-from pydantic import Field
-
-from tapeagents.agent import Agent, Node
-from tapeagents.core import Action, AgentStep, LLMOutputParsingFailureAction, PartialStep, SetNextNode, Step, StopStep
+from tapeagents.agent import Agent
+from tapeagents.core import (
+    Action,
+    AgentStep,
+    LLMOutputParsingFailureAction,
+    PartialStep,
+    SetNextNode,
+    Step,
+    StopStep,
+)
+from tapeagents.dialog_tape import UserStep
 from tapeagents.llms import LLM, LLMStream
-from tapeagents.nodes import MonoNode
+from tapeagents.nodes import StandardNode
 from tapeagents.tools.simple_browser import PageObservation
 from tapeagents.utils import FatalError
 
 from .prompts import PromptRegistry
 from .steps import (
-    WebAgentStep,
-    WebStep,
+    ReasoningThought,
+    ReflectionThought,
+    WebAgentAction,
     WebTape,
+    WebTask,
 )
 
 
-class WebNode(MonoNode):
-    max_retries: int = 3
-    max_same_action: int = 4
-    current_retries: int = 0
+class WebNode(StandardNode):
+    max_retries: int = 3  # max number of times to retry the current node if the last step is an LLMOutputParsingFailureAction
+    current_retries: int = 0  # current number of retries for the current node
+    max_same_action: int = 4  # max number of times to repeat the same action
+    max_chars_page_observation: int = 2000  # max number of characters to keep in PageObservation["text"]
 
-    # def trim_tape(self, tape: WebTape) -> WebTape:
-    #     """
-    #     Trim all page observations except the last two.
-
-    #     Args:
-    #         tape (Tape): The tape object to be trimmed.
-
-    #     Returns:
-    #         Tape: The trimmed tape object.
-    #     """
-    #     tape = super().prepare_tape(tape)  # type: ignore
-    #     page_positions = [i for i, step in enumerate(tape.steps) if isinstance(step, PageObservation)]
-    #     if len(page_positions) < 2:
-    #         return tape
-    #     prev_page_position = page_positions[-2]
-    #     steps = []
-    #     for step in tape.steps[:prev_page_position]:
-    #         if isinstance(step, PageObservation):
-    #             short_text = f"{step.text[:max_chars]}\n..." if len(step.text) > max_chars else step.text
-    #             new_step = step.model_copy(update=dict(text=short_text))
-    #         else:
-    #             new_step = step
-    #         steps.append(new_step)
-    #     trimmed_tape = tape.model_copy(update=dict(steps=steps + tape.steps[prev_page_position:]))
-    #     return trimmed_tape
 
     def tape_to_messages(self, tape: WebTape, steps_description: str) -> list[dict]:
         """
         Converts a Tape object and steps description into a list of messages for LLM conversation.
 
-        Modifications from the original MonoNode:
+        Modifications from the original StandardNode:
         - If the last n steps are LLMOutputParsingFailureAction, put the guidance before the error steps
           and add a new guidance message that asks to retry.
-        - Otherwise, remove all LLMOutputParsingFailureAction steps and use the default behavior on the cleaned tape.
+        - Do not render older LLMOutputParsingFailureAction steps that got solved.
+        - Always render WebTask observations as they contain the instructions
+        - Truncate PageObservation steps up to `max_chars_page_observation` chars (instead of 100 by default).
 
         Args:
             tape (Tape): A Tape object containing conversation steps.
@@ -71,34 +59,55 @@ class WebNode(MonoNode):
                        Messages from tape are added with roles based on step type.
                        If guidance exists, it's added as the final user message.
         """
-        if len(tape.steps) > 0 and isinstance(tape.steps[-1], LLMOutputParsingFailureAction):
-            messages: list[dict] = []
-            if self.system_prompt:
-                messages.append({"role": "system", "content": self.system_prompt})
-            if steps_description:
-                messages.append({"role": "user", "content": steps_description})
-            # ignore the last n consecutive parsing error steps for now, will add them later
-            n_parsing_errors = 0
-            for step in reversed(tape.steps):
-                if isinstance(step, LLMOutputParsingFailureAction):
-                    n_parsing_errors += 1
+        messages: list[dict] = []
+        if self.system_prompt:
+            messages.append({"role": "system", "content": self.system_prompt})
+        if steps_description:
+            messages.append({"role": "user", "content": steps_description})
+        # ignore the last n consecutive parsing error steps for now, will add them later
+        last_parsing_error_steps: list[LLMOutputParsingFailureAction] = []
+        for step in reversed(tape.steps):
+            if isinstance(step, LLMOutputParsingFailureAction):
+                last_parsing_error_steps.append(step)
+            else:
+                break
+        # put back the last n parsing error steps in the original order
+        last_parsing_error_steps.reverse()
+        # remove all the parsing error steps
+        cleaned_steps = [step for step in tape.steps if not isinstance(step, LLMOutputParsingFailureAction)]
+        # get the positions of all observations to trim the old ones
+        page_observation_idx = [i for i, step in enumerate(cleaned_steps) if isinstance(step, PageObservation)]
+        for i, step in enumerate(cleaned_steps):
+            role = "assistant" if isinstance(step, AgentStep) else "user"
+            if isinstance(step, PageObservation):
+                if i not in page_observation_idx[-self.trim_obs_except_last_n:]:
+                    # skip old page observations
+                    continue
                 else:
-                    break
-            for step in tape.steps[:-n_parsing_errors]:
-                role = "assistant" if isinstance(step, AgentStep) else "user"
-                messages.append({"role": role, "content": step.llm_view()})
-            # add the initial guidance message
-            if self.guidance:
-                messages.append({"role": "user", "content": self.guidance})
-            # add the last n parsing error steps and the new guidance message that asks to retry
-            for step in tape.steps[-n_parsing_errors:]:
-                messages.append({"role": "assistant", "content": step.llm_view()})
-                messages.append({"role": "user", "content": "You made a mistake. Look at your generation (in 'llm_output') and the error message (in 'error') and please try again."})
-            return messages
-        else:
-            steps_without_parsing_errors = [step for step in tape.steps if not isinstance(step, LLMOutputParsingFailureAction)]
-            cleaned_tape = tape.model_copy(update=dict(steps=steps_without_parsing_errors))
-            return super().tape_to_messages(cleaned_tape, steps_description)
+                    # adaptation from step.short_view()
+                    view = step.llm_dict()
+                    del view["kind"]  # remove the kind field
+                    if len(view["text"]) > self.max_chars_page_observation:
+                        view["text"] = view["text"][:self.max_chars_page_observation] + "..."
+                    view = json.dumps(view, indent=2, ensure_ascii=False)
+                    view = f"Page Observation: ```json\n{view}\n```"
+            elif isinstance(step, WebTask):
+                view = f"Task: {step.task}"
+            elif isinstance(step, UserStep):
+                view = step.content
+            elif isinstance(step, ReasoningThought):
+                view = step.reasoning
+            else:
+                view = step.llm_view()
+            messages.append({"role": role, "content": view})
+        # add the initial guidance message
+        if self.guidance:
+            messages.append({"role": "user", "content": self.guidance})
+        # add the last n parsing error steps and the new guidance message that asks to retry
+        for step in last_parsing_error_steps:
+            messages.append({"role": "assistant", "content": step.llm_view()})
+            messages.append({"role": "user", "content": "You made a mistake. Look at your generation (in 'llm_output') and the error message (in 'error') and please try again."})
+        return messages
 
     def generate_steps(
         self, agent: Agent, tape: WebTape, llm_stream: LLMStream
@@ -109,9 +118,10 @@ class WebNode(MonoNode):
         This method processes the output from a language model stream and converts it into a series of steps.
         It handles the parsing of completions and post-processing of steps.
 
-        Modifications from the original MonoNode:
+        Modifications from the original StandardNode:
         - If the last step is an LLMOutputParsingFailureAction, retry the current node
-          up to max_retries times. Otherwise, use the default node selection logic.
+          **up to max_retries times**.
+        - If the last action is repeated more than **max_same_action times**, stop the task.
 
         Args:
             agent (Any): The agent instance that will execute the steps.
@@ -134,45 +144,45 @@ class WebNode(MonoNode):
         n_last_actions = len(list(takewhile(lambda x: x == last_action, reversed(previous_actions))))
 
         new_steps = []
-        try:
-            cnt = 0
-            for event in llm_stream:
-                if event.output:
-                    cnt += 1
-                    assert event.output.content
-                    for step in self.parse_completion(event.output.content, llm_stream.prompt.id):
-                        step = self.postprocess_step(tape, new_steps, step)
-                        # check that the new step is not a repetition of the last action up to max_same_action times
-                        if isinstance(step, Action):
-                            if step == last_action:
-                                n_last_actions += 1
-                                if n_last_actions > self.max_same_action:
-                                    raise FatalError(f"Max same action reached! {step}")
-                            else:
-                                n_last_actions = 1
-                                last_action = step
+        for event in llm_stream:
+            if not event.output:
+                continue
+            if event.output.content:
+                new_steps += list(self.parse_completion(event.output.content))
+            if event.output.tool_calls and self.use_function_calls:
+                new_steps += [self.tool_call_to_step(tool_call) for tool_call in event.output.tool_calls]
 
-                        new_steps.append(step)
-                        yield step
-
-                        # if the last step was an LLMOutputParsingFailureAction, retry the current node up to max_retries times
-                        if isinstance(step, LLMOutputParsingFailureAction):
-                            if self.current_retries < self.max_retries:
-                                retry_step = SetNextNode(next_node=self.name)
-                                new_steps.append(retry_step)
-                                yield retry_step
-                                self.current_retries += 1
-                                break
-                            else:
-                                raise FatalError(f"Max retries reached for node {self.name}!")
-                        else:
-                            self.current_retries = 0
-            if not cnt:
-                raise FatalError("No completions!")
-        except FatalError:
-            raise
-
-        if self.next_node and not isinstance(new_steps[-1], StopStep) and not isinstance(new_steps[-1], SetNextNode):
+            for i, step in enumerate(new_steps):
+                step = self.postprocess_step(tape, new_steps[:i], step)
+                yield step
+                # check that the new step is not a repetition of the last action up to max_same_action times
+                if isinstance(step, Action):
+                    if step == last_action:
+                        n_last_actions += 1
+                        if n_last_actions > self.max_same_action:
+                            raise FatalError(f"Max same action reached! {step}")
+                    else:
+                        n_last_actions = 1
+                        last_action = step
+                # if the last step was an LLMOutputParsingFailureAction, retry the current node up to max_retries times
+                if isinstance(step, LLMOutputParsingFailureAction):
+                    if self.current_retries < self.max_retries:
+                        retry_step = SetNextNode(next_node=self.name)
+                        yield retry_step
+                        self.current_retries += 1
+                        new_steps.append(retry_step)
+                        break
+                    else:
+                        raise FatalError(f"Max retries reached for node {self.name}!")
+                else:
+                    self.current_retries = 0
+        if not new_steps:
+            raise FatalError("No completions!")
+        if (
+            self.next_node
+            and not isinstance(new_steps[-1], StopStep)
+            and not any(isinstance(step, SetNextNode) for step in new_steps)
+        ):
             yield SetNextNode(next_node=self.next_node)
 
 class WebAgent(Agent):
@@ -186,21 +196,27 @@ class WebAgent(Agent):
                     guidance=PromptRegistry.start,
                     system_prompt=PromptRegistry.system_prompt,
                     steps_prompt=PromptRegistry.allowed_steps,
-                    agent_steps=WebAgentStep,
+                    steps=ReasoningThought,
+                    trim_obs_except_last_n=3,  # keep the last 3 observations from the tape in prompt messages
+                    max_chars_page_observation=2000,  # keep up to 2000 chars in PageObservation steps
                 ),
                 WebNode(
                     name="reflect",
                     guidance=PromptRegistry.reflect,
                     system_prompt=PromptRegistry.system_prompt,
                     steps_prompt=PromptRegistry.allowed_steps,
-                    agent_steps=WebAgentStep,
+                    steps=ReflectionThought,
+                    trim_obs_except_last_n=3,  # keep the last 3 observations from the tape in prompt messages
+                    max_chars_page_observation=2000,  # keep up to 2000 chars in PageObservation steps
                 ),
                 WebNode(
                     name="act",
                     guidance=PromptRegistry.act,
                     system_prompt=PromptRegistry.system_prompt,
                     steps_prompt=PromptRegistry.allowed_steps,
-                    agent_steps=WebAgentStep,
+                    steps=WebAgentAction,
+                    trim_obs_except_last_n=3,  # keep the last 3 observations from the tape in prompt messages
+                    max_chars_page_observation=2000,  # keep up to 2000 chars in PageObservation steps
                     next_node="reflect",
                 ),
             ],
