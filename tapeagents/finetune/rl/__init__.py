fix entropy computation

tapeagents/finetune/rl/__init__.py
@@ -11,14 +11,9 @@ import torch.nn.functional as F
 from datasets import Dataset
 from transformers import BatchEncoding, PreTrainedModel
 
-from .utils import (
-    StepConfig,
-    calculate_advantage,
-    calculate_rewards_with_implicit_kl,
-    masked_mean,
-    masked_sum,
-    replace_dataset_column,
-)
+from .utils import (StepConfig, calculate_advantage,
+                    calculate_rewards_with_implicit_kl, masked_mean,
+                    masked_sum, replace_dataset_column)
 
 # FIXME: remove a warnings, but might be worth investigating
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -98,13 +93,15 @@ def rl_step(model: PreTrainedModel, batch: dict, config: RLConfig) -> tuple[torc
         attention_mask=batch["attention_mask"],
         labels=batch["labels"],
     )
-
+    logits = outputs.logits[:, :-1, :]
+    logprobs = F.log_softmax(logits, dim=-1)
+    probs = F.softmax(logits, dim=-1)
+    entropy = -(probs * logprobs).sum(dim=-1)
     new_log_probs = torch.gather(
-        F.log_softmax(outputs.logits[:, :-1, :], dim=-1),  # the last log probs has no target
+       logprobs,  # the last log probs has no target
         dim=2,
         index=batch["input_ids"][:, 1:].unsqueeze(2),
     ).squeeze(2)
-    entropy = -(torch.exp(new_log_probs) * new_log_probs).sum(dim=-1)
     assert torch.isfinite(new_log_probs).all(), f"new_log_probs is not finite: {new_log_probs}"
 
     masks_ = masks[:, 1:]